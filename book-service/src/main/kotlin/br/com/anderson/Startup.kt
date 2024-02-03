32bbd38d0d8b6df3ad6da6d6c3b9bcea0e5ec283
Anderson Silva <contatoahenrique@gmail.com>
2024-02-03 02:58:05 -0300

    Add zookeeper

--- book-service/src/main/kotlin/br/com/anderson/Startup.kt
+++ book-service/src/main/kotlin/br/com/anderson/Startup.kt
@@ -2,10 +2,12 @@ package br.com.anderson
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 class BookServiceApplication
 
 fun main(args: Array<String>) {
